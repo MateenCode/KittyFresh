dd78d3d02dd038204e55a521f7cb8af973def233
Mateen Kazia <mateencode@gmail.com>
2020-01-26 21:59:45 -0600

    added checkout page, checkout item compenent

--- src/App.js
+++ src/App.js
@@ -8,6 +8,8 @@ import "./sass/App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from "./pages/checkout/checkout.component";
+
 import Header from "./components/header/header.component";
 import { auth, createUserProfileDocument } from "./firebase/firebase.utils";
 import { setCurrentUser } from "./redux/user/user.actions";
@@ -43,6 +45,7 @@ export class App extends PureComponent {
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
+          <Route exact path='/checkout' component={CheckoutPage} />
           <Route
             exact
             path='/signin'
