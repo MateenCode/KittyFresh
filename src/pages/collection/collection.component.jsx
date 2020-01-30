caaab2ea307673d5dfb3ccc3c71cf91b32e31c39
Mateen Kazia <mateencode@gmail.com>
2020-01-29 19:18:38 -0600

    Improving Naming Of Component

---
+++ src/pages/collection/collection.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import CollectionItem from "../../components/collection-item/collection-item.component";
+
+import "./collection.styles.scss";
+
+function collectionPage({ match }) {
+  return (
+    <div className='collection-page'>
+      <h2>Collection Page</h2>
+    </div>
+  );
+}
+
+export default collectionPage;
