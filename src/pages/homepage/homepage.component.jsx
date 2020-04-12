4e576db57e4b45bed35cdbb871f45aeea8ac2d49
Mateen Kazia <mateencode@gmail.com>
2020-04-12 18:24:14 -0500

    banner updated

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -7,7 +7,7 @@ import { HomePageContainer, Banner } from "./homepage.styles";
 const HomePage = () => (
   <HomePageContainer>
     <Banner
-      src='https://i.imgur.com/6NzHfHR.gif'
+      src='https://images.contentful.com/q602vtcuu3w3/4SjR5pqQAcBPE4HWejWlB2/fbea941a3ee4cefce8dfd1e3a83f7a7b/40OFF_WAPP_WACC_WINT7.jpg?q=80&w=2160&fm=webp'
       alt='banner'
       href='http://localhost:3000/shop/womens'
     />
