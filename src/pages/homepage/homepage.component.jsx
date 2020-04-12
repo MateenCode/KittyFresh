29975419e1b5ce31ea85b05a080260e549560590
Mateen Kazia <mateencode@gmail.com>
2020-04-11 20:01:22 -0500

    banner added

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -2,10 +2,15 @@ import React from "react";
 
 import Directory from "../../components/directory/directory.component";
 
-import { HomePageContainer } from "./homepage.styles";
+import { HomePageContainer, Banner } from "./homepage.styles";
 
 const HomePage = () => (
   <HomePageContainer>
+    <Banner
+      src='https://i.imgur.com/6NzHfHR.gif'
+      alt='banner'
+      href='http://localhost:3000/shop/womens'
+    />
     <Directory />
   </HomePageContainer>
 );
--- src/pages/homepage/homepage.styles.jsx
+++ src/pages/homepage/homepage.styles.jsx
@@ -6,3 +6,9 @@ export const HomePageContainer = styled.div`
   align-items: center;
   padding: 20px 80px;
 `;
+
+export const Banner = styled.img`
+  width: 100%;
+  margin: 0px 15px 20px 15px;
+  cursor: pointer;
+`;
